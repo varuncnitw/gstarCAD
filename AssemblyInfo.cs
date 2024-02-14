5f8628c79e91a1bc1cab04bbfc9db8d47a0f0d5a
varuncnitw <varuncnitw@gmail.com>
2024-02-14 11:57:05 +0530

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Please refer to "COPYRIGHT.md" for the relevant copyright statement of this software.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("hello")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hello")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("119a1d12-aebf-44b3-94eb-3e168c43d1c8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Hello.cs
@@ -0,0 +1,84 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Please refer to "COPYRIGHT.md" for the relevant copyright statement of this software.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+
+using GrxCAD.Runtime;
+using GrxCAD.ApplicationServices;
+using GrxCAD.DatabaseServices;
+using GrxCAD.Geometry;
+//Imports Autodesk.AutoCAD.Runtime
+//Imports Autodesk.AutoCAD.ApplicationServices
+//Imports Autodesk.AutoCAD.DatabaseServices
+//Imports Autodesk.AutoCAD.Geometry
+
+[assembly: CommandClass(typeof(hello.HelloCmd))]
+
+namespace hello
+{
+  public class HelloCmd
+  {
+    [CommandMethod("Hello")]
+    static public void DoIt()
+    {
+
+      try
+      {
+        //Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Hello dotnet");
+                /*              Dim acDoc As Document =*/
+                Document acDoc = Application.DocumentManager.MdiActiveDocument;
+                Database acCurDb = acDoc.Database;
+
+                using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+                {
+                    // Open the Layer table for read
+                    LayerTable acLyrTbl;
+                    acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+                                                    OpenMode.ForRead) as LayerTable;
+
+                    string sLayerName = "VARUNLAYERTURNON";
+                    if (acLyrTbl.Has(sLayerName) == false)
+                    {
+                        using (LayerTableRecord acLyrTblRec = new LayerTableRecord())
+                        {
+                            // Assign the layer a name
+                            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("when did the layer select dotnet");
+                            acLyrTblRec.Name = sLayerName;
+
+                            // Turn the layer off
+                            acLyrTblRec.IsOff = false;
+
+                            // Upgrade the Layer table for write
+                            acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForWrite);
+
+                            // Append the new layer to the Layer table and the transaction
+                            acLyrTbl.Add(acLyrTblRec);
+                            acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Hello did the layer select dotnet");
+
+                        }
+                    }
+                    else
+                    {
+                        LayerTableRecord acLyrTblRec = acTrans.GetObject(acLyrTbl[sLayerName],
+                                                        OpenMode.ForWrite) as LayerTableRecord;
+
+                        // Turn the layer off
+                        acLyrTblRec.IsOff = true;
+                        Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Hello off dotnet");
+
+                    }
+                    acTrans.Commit();
+                
+                }
+
+      }
+            catch (System.Exception ex)
+      {
+        System.Windows.Forms.MessageBox.Show(ex.ToString());
+      }
+    }
+  }
+}
