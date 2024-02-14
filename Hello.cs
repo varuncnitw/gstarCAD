d14dcf8b71c8e09964d05489dd8f1ae250715097
varuncnitw <varuncnitw@gmail.com>
2024-02-14 14:37:13 +0530

    Add files via upload

--- Hello.cs
+++ Hello.cs
@@ -9,6 +9,7 @@ using GrxCAD.Runtime;
 using GrxCAD.ApplicationServices;
 using GrxCAD.DatabaseServices;
 using GrxCAD.Geometry;
+using System.IO;
 //Imports Autodesk.AutoCAD.Runtime
 //Imports Autodesk.AutoCAD.ApplicationServices
 //Imports Autodesk.AutoCAD.DatabaseServices
@@ -28,9 +29,27 @@ namespace hello
       {
         //Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("Hello dotnet");
                 /*              Dim acDoc As Document =*/
-                Document acDoc = Application.DocumentManager.MdiActiveDocument;
+               // Document acDoc = Application.DocumentManager.MdiActiveDocument;
+                //Database acCurDb = acDoc.Database;
+                Document acDoc = Application.DocumentManager.Open("E:\\DDRive\\CBIT\\gstarCAD\\Innersixangleflatcountersunkheadscrews(B).dwg",
+                    false, null);
                 Database acCurDb = acDoc.Database;
 
+                // Open the Block table for read
+                //BlockTable acBlkTbl = null;
+                //acBlkTbl = acCurDb.BlockTableId.Open(OpenMode.ForRead) as BlockTable;
+                //// Open the Block table record Model space for read
+                //BlockTableRecord acBlkTblRec = null;
+
+                //acBlkTblRec = acBlkTbl[BlockTableRecord.ModelSpace].Open(OpenMode.ForRead) as BlockTableRecord;
+
+                //    foreach (ObjectId acObjId in acBlkTblRec)
+                //{
+                //    acDoc.Editor.WriteMessage("\nDXF name: " + acObjId.ObjectClass.DxfName);
+                //    acDoc.Editor.WriteMessage("\nObjectID: " + acObjId.ToString());
+                //    acDoc.Editor.WriteMessage("\nHandle: " + acObjId.Handle.ToString());
+                //    acDoc.Editor.WriteMessage("\n");
+                //}
                 using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
                 {
                     // Open the Layer table for read
